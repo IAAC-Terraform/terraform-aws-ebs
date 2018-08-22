770ec37deab9602a3759c02c82f5dc55d92daf81
Raghavan R <raghavan.r@capgemini.com>
2018-08-22 22:51:41 +1000

    syntax error

--- ebs.tf
+++ ebs.tf
@@ -1,5 +1,5 @@
 resource "aws_ebs_volume" "ebs" {
-    count = "${var.create} ? 1 : 0}"
+    count = "${var.create ? 1 : 0}"
     availability_zone = "${var.availability_zone}"
     encrypted = "${var.encrypted}"
     kms_key_id = "${var.kms_key_id}"
